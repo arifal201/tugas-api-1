3824a6ac93fec05c28456842459e638b2533dc85
Zainal Mustofa <zainalmustof@gmail.com>
2019-10-26 23:06:01 +0700

    create chartjs

--- app/controllers/plantation_controller_controller.rb
+++ app/controllers/plantation_controller_controller.rb
@@ -2,4 +2,9 @@ class PlantationControllerController < ApplicationController
   def index
     @plantationmodel = PlantationModel.all
   end
+  
+  def chart
+    @plantationmodel = PlantationModel.all
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'plantation_controller/index'
   root 'plantation_controller#index'
+  get 'plantation_controller/chart', to: 'plantation_controller#chart' 
   resources :plantation_controller
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
